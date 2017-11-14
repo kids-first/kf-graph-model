29789530488eef0b1cc8d4a07e802bbf3d659a52
Junjun Zhang <junjun.ca@gmail.com>
2017-11-13 23:36:14 -0500

    added schema for HPO terms, added genetic_testing_status etc

--- tests/test_schema_list.py
+++ tests/test_schema_list.py
@@ -2,6 +2,7 @@ def test_schema_list(graph_schemas):
     all_schemas = set(graph_schemas.schemas)
     expected_schemas = {'_definitions', 'program', 'project', 'sequence_alignment',
                         'mutect2_ssm_calling', 'aliquot', 'sample', 'case', 'exposure',
-                        'family', 'read_group', 'read_group_qc'}
+                        'family', 'read_group', 'read_group_qc', 'hpo',
+                        'genetic_testing_status'}
 
     assert all_schemas == expected_schemas
