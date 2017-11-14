82279a948f55210dbf5f880db8da7f8f9f688fb4
Junjun Zhang <junjun.ca@gmail.com>
2017-11-14 17:44:59 -0500

    added more cardiac_abnormality, and linked it with HPO term

--- tests/test_schema_list.py
+++ tests/test_schema_list.py
@@ -3,6 +3,6 @@ def test_schema_list(graph_schemas):
     expected_schemas = {'_definitions', 'program', 'project', 'sequence_alignment',
                         'mutect2_ssm_calling', 'aliquot', 'sample', 'case', 'exposure',
                         'family', 'read_group', 'read_group_qc', 'hpo',
-                        'genetic_testing_status'}
+                        'genetic_testing_status', 'cardiac_abnormality'}
 
     assert all_schemas == expected_schemas
